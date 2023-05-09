b323dda93df120cdcae6390705d12ac4a1ef49d5
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-05-09 23:47:24 +0900

    一部を除きテスト通った

---
+++ d2_32x2.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <cstdint>
+#include <utility>
+#include <bit>
+#include <format>
+#include <iostream>
+
+struct large_int
+{
+    std::uint32_t lo_;
+    std::int32_t hi_;
+};
+
+struct u32_pair
+{
+    std::uint32_t lo_, hi_;
+    constexpr bool is_zero() const
+    {
+        return (hi_ & 0x7fff'ffff) == 0 && lo_ == 0;
+    }
+    constexpr int neg() const
+    {
+        return !!(hi_ & 0x8000'0000);
+    }
+    constexpr std::int32_t exp() const
+    {
+        return (hi_ >> 20) & 2047;
+    }
+    constexpr std::uint32_t hfrac() const
+    {
+        return (hi_ & 1048575) + (1u << 20);
+    }
+};
+
+inline std::uint32_t shift(std::uint32_t v, int x)
+{
+    if (x <= -32)
+    {
+        return 0;
+    }
+    if (x < 0)
+    {
+        return v >> (-x);
+    }
+    if (x < 32)
+    {
+        return v << x;
+    }
+    return 0;
+}
+
+inline std::pair<large_int, bool> d2_32x2(double x)
+{
+    auto up = std::bit_cast<u32_pair>(x);
+    if (up.is_zero())
+    {
+        return {{0, 0}, true};
+    }
+    auto f0 = up.hfrac();
+    auto f1 = up.lo_;
+    auto e = up.exp() + (-52 - 1023);
+    large_int r = [&]()->large_int {
+        if (0 <= e)
+        {
+            return {shift(f1, e),
+                        std::int32_t(shift(f0, e) + shift(f1, e - 32))};
+        }
+        else
+        {
+            return {shift(f1, e) + shift(f0, e + 32),
+                        std::int32_t(shift(f0, e))};
+        }
+    }();
+    if (up.neg()){
+        std::int32_t in = -1;
+        return {{ -r.lo_, in-r.hi_ }, true };
+    } else {
+        return { r, true };
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "d2_32x2.h"
+#include <bit>
+#include <format>
+#include <iostream>
+
+void test_one( double x ){
+    auto [i,ok_a] = d2_32x2(x);
+    auto a = std::bit_cast<std::int64_t>(i);
+    auto e = static_cast<std::int64_t>(x);
+    auto ok_e = std::abs(x)<static_cast<double>(1ull<<63);
+
+    if (ok_a && ok_e){
+        if (a != e){
+            std::cout << std::format( "x={} a={:x} e={:x}", x, a, e ) << std::endl;
+            return;
+        }
+    }
+    if (ok_a != ok_e ){
+        std::cout << std::format( "ok_a={}  ok_e={}", ok_a, ok_e ) << std::endl;
+        return;
+    }
+    std::cout << std::format( "okay: x={}", x ) << std::endl;
+}
+
+void test( double x ){
+    test_one(x);
+    test_one(-x);
+}
+
+
+int main(){
+    test( 0.0 );
+    test( 1 );
+    test( 255 );
+    test( 256 );
+    test( 0xaa55 );
+    test( 65535 );
+    test( 65536 );
+    for( int i=17; i<56 ; ++i ){
+        test( (1ull<<i) );
+        test( (1ull<<i)-1 );
+    }
+    return 0;
+}
