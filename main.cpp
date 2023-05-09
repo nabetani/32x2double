3639cbc8f2988efa68d74adbe4a1af5c6f07bc33
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-05-10 08:15:54 +0900

    テスト追加

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <format>
 #include <iostream>
 #include <limits>
+#include <cmath>
 
 int test_one(double x)
 {
@@ -33,32 +34,49 @@ int test(double x)
     return test_one(x) + test_one(-x);
 }
 
+int testd(double x)
+{
+    auto n = std::nextafter(x, x * 2);
+    auto p = std::nextafter(x, x / 2);
+    return test(x) + test(n) + test(p);
+}
+
 int main()
 {
-    int failed=0;
+    int failed = 0;
     failed += test(0.0);
     failed += test(1);
     failed += test(255);
     failed += test(256);
     failed += test(0xaa55);
+    failed += test(0x1234'5678'abcd'2468);
     failed += test(65535);
     failed += test(65536);
     for (int i = 17; i < 63; ++i)
     {
-       std::cout << std::format("***** i={}", i) << std::endl;
+        std::cout << std::format("***** i={}", i) << std::endl;
         failed += test((1ull << i));
         failed += test((1ull << i) - 1);
         failed += test((1ull << i) + 1);
         failed += test((1ull << i) + (1ull << 31));
-        if (31<i){
+        if (31 < i)
+        {
             failed += test((1ull << i) - (1ull << 31));
         }
     }
+    constexpr auto p62 = static_cast<double>(1ull << 62);
+    failed += testd(p62);
+    failed += testd(p62 * 2);
+    failed += testd(p62 * 4);
+    failed += testd(p62 * 8);
+    failed += testd(p62 * 16);
+    failed += testd(p62 * 32);
+
     using lim = std::numeric_limits<double>;
-    failed += test( lim::infinity() );
-    failed += test( lim::quiet_NaN() );
-    failed += test( lim::signaling_NaN() );
-    failed += test( lim::denorm_min() );
+    failed += testd(lim::infinity());
+    failed += testd(lim::quiet_NaN());
+    failed += testd(lim::signaling_NaN());
+    failed += testd(lim::denorm_min());
 
     std::cout << std::format("failed={}", failed) << std::endl;
 
