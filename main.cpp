6075362277f5e6694aa0ff0459fb558232c66a3a
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-05-10 07:56:57 +0900

    失敗数を出す

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <format>
 #include <iostream>
 
-void test_one(double x)
+int test_one(double x)
 {
     auto [i, ok_a] = d2_32x2(x);
     auto a = std::bit_cast<std::int64_t>(i);
@@ -15,42 +15,45 @@ void test_one(double x)
         if (a != e)
         {
             std::cout << std::format("x={} a={:x} e={:x}", x, a, e) << std::endl;
-            return;
+            return 1;
         }
     }
     if (ok_a != ok_e)
     {
         std::cout << std::format("x={} e={:x} ok_a={}  ok_e={}", x, e, ok_a, ok_e) << std::endl;
-        return;
+        return 1;
     }
     std::cout << std::format("okay: x={}", x) << std::endl;
+    return 0;
 }
 
-void test(double x)
+int test(double x)
 {
-    test_one(x);
-    test_one(-x);
+    return test_one(x) + test_one(-x);
 }
 
 int main()
 {
-    test(0.0);
-    test(1);
-    test(255);
-    test(256);
-    test(0xaa55);
-    test(65535);
-    test(65536);
+    int failed=0;
+    failed += test(0.0);
+    failed += test(1);
+    failed += test(255);
+    failed += test(256);
+    failed += test(0xaa55);
+    failed += test(65535);
+    failed += test(65536);
     for (int i = 17; i < 63; ++i)
     {
        std::cout << std::format("***** i={}", i) << std::endl;
-        test((1ull << i));
-        test((1ull << i) - 1);
-        test((1ull << i) + 1);
-        test((1ull << i) + (1ull << 31));
+        failed += test((1ull << i));
+        failed += test((1ull << i) - 1);
+        failed += test((1ull << i) + 1);
+        failed += test((1ull << i) + (1ull << 31));
         if (31<i){
-            test((1ull << i) - (1ull << 31));
+            failed += test((1ull << i) - (1ull << 31));
         }
     }
+    std::cout << std::format("failed={}", failed) << std::endl;
+
     return 0;
 }
